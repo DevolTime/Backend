7a13fae5ef6601035f0594aeb8bd66979d63386d
Kevindresbar <kevindresbar@gmail.com>
2026-05-20 21:51:32 -0500

    mongoose model, creacion rutas, exportar funciones

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import {  mongoose, Schema} from "mongoose";
+
+// 1era parte para definir el esquema 
+
+const userShema = new Schema({},{});
+
+//2da parte definir el modelo 
+const userShema = model ();
+
+
+export default userShema;
